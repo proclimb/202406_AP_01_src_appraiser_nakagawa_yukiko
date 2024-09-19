c7c20401e32872141a05d05fb12a35108713981c
nakagawayukiko <202406ap01_nakagawa@x-ide.sakura.ne.jp>
2024-09-19 16:13:36 +0900

    タイトル管理 No.34

--- SRC/js/title.js
+++ SRC/js/title.js
@@ -16,39 +16,39 @@ function fnFTitleEditCheck(zero) {
 		alert('表示順は2文字以内の半角数字で入力してください');
 		return;
 	}
-}
 
-tmp = form.seqNo.value;
-if (zero !== 0) {
+
+	tmp = form.seqNo.value;
+	if (zero !== 0) {
+		if (tmp.length == 0) {
+			alert('表示順を入力してください');
+			return;
+		}
+		if (tmp == 0) {
+			alert('0以外の数字を入力してください');
+			return;
+		}
+		if (tmp.length > 2 || tmp.match(/[^0-9]+/)) {
+			alert('表示順は2文字以内の半角数字で入力してください');
+			return;
+		}
+	}
+
+	tmp = form.name.value;
 	if (tmp.length == 0) {
-		alert('表示順を入力してください');
+		alert('名前を入力してください');
 		return;
 	}
-	if (tmp == 0) {
-		alert('0以外の数字を入力してください');
+	if (tmp.length > 100) {
+		alert('名前は100文字以内で入力してください');
 		return;
 	}
-	if (tmp.length > 2 || tmp.match(/[^0-9]+/)) {
-		alert('表示順は2文字以内の半角数字で入力してください');
-		return;
+
+	if (confirm('この内容で登録します。よろしいですか？')) {
+		form.act.value = 'fTitleEditComplete';
+		form.submit();
 	}
 }
-
-tmp = form.name.value;
-if (tmp.length == 0) {
-	alert('名前を入力してください');
-	return;
-}
-if (tmp.length > 100) {
-	alert('名前は100文字以内で入力してください');
-	return;
-}
-
-if (confirm('この内容で登録します。よろしいですか？')) {
-	form.act.value = 'fTitleEditComplete';
-	form.submit();
-}
-
 
 
 
